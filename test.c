f65f3b71ba4a2151c7af70ed9952b50389610504
Yubo.Wang <wangyubo9661@qq.com>
2020-03-25 11:26:25 +0800

    规范代码格式并添加字符输入

--- test.c
+++ test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main()
+#include <string.h>
+
+int main(void)
 {
     printf("hello world!\n");
     printf("Yubo.Wang\n");
@@ -8,5 +10,8 @@ int main()
     printf("try try try! \n");
     printf("gpg off\n");
     printf("Hello there!\n");
+    
+    getchar();
+
     return 0;
 }
